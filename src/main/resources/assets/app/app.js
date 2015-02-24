c60e34dfc428d8a1d51c66bcf054ee0d2f30330b
spinetrak <spinetrak@spinetrak.net>
2015-02-24 23:55:20 +0100

    - code cleanup - hide login/register/profile forms depending on state

--- src/main/resources/assets/app/app.js
+++ src/main/resources/assets/app/app.js
@@ -1,31 +1,31 @@
 requirejs.config({
-  paths: {
-    'text': '../lib/require/text',
-    'durandal':'../lib/durandal/js',
-    'plugins' : '../lib/durandal/js/plugins',
-    'transitions' : '../lib/durandal/js/transitions',
-    'knockout': '../lib/knockout/knockout-3.1.0',
-    'jquery': '../lib/jquery/jquery-1.9.1',
-	'services': 'services'
-    } 
+    paths: {
+        'text': '../lib/require/text',
+        'durandal': '../lib/durandal/js',
+        'plugins': '../lib/durandal/js/plugins',
+        'transitions': '../lib/durandal/js/transitions',
+        'knockout': '../lib/knockout/knockout-3.1.0',
+        'jquery': '../lib/jquery/jquery-1.9.1',
+        'services': 'services'
+    }
 });
- 
+
 define(function (require) {
-   var system = require('durandal/system'),
-       app = require('durandal/app'),
-	   dialog = require('plugins/dialog');
- 
-   system.debug(true);
+    var system = require('durandal/system'),
+        app = require('durandal/app'),
+        dialog = require('plugins/dialog');
+
+    system.debug(true);
 
     app.title = 'spinetrak::gasguzzler';
- 
-   app.configurePlugins({
-     router:true,
-     dialog: true
-   });
-	
-   app.start().then(function() {
-       dialog.MessageBox.setDefaults({class: "notice success", buttonClass: "small green float-right"});
-     app.setRoot('services/shell');
-   });
+
+    app.configurePlugins({
+        router: true,
+        dialog: true
+    });
+
+    app.start().then(function () {
+        dialog.MessageBox.setDefaults({class: "notice success", buttonClass: "small green float-right"});
+        app.setRoot('services/shell');
+    });
 });
--- src/main/resources/assets/app/services/shell.js
+++ src/main/resources/assets/app/services/shell.js
@@ -3,30 +3,14 @@ define(function (require) {
 
     var routeArr = [
         {route: '', title: 'Home', moduleId: 'home/home', nav: true},
-        {route: 'recentstats', title: 'Recent Stats', moduleId: 'site/recentstats', nav: true},
-        {route: 'login', title: 'Login/Register', moduleId: 'user/login', nav: !sessionStorage.getItem("token")},
-        {route: 'profile', title: 'Profile', moduleId: 'user/profile', nav: sessionStorage.getItem("token")}
+        {route: 'recentstats', title: 'Stats', moduleId: 'site/recentstats', nav: true},
+        {route: 'user', title: 'You', moduleId: 'user/user', nav: true}
     ];
 
     return {
         router: router,
         activate: function () {
             router.map(routeArr).buildNavigationModel();
-            return router.activate();
-        },
-
-        reconfigure: function () {
-            router.deactivate();
-
-            var myroutes = [
-                {route: '', title: 'Home', moduleId: 'home', nav: true},
-                {route: 'recentstats', title: 'Recent Stats', moduleId: 'recentstats', nav: true},
-                {route: 'login', title: 'Login', moduleId: 'login', nav: !sessionStorage.getItem("token")},
-                {route: 'profile', title: 'Profile', moduleId: 'profile', nav: sessionStorage.getItem("token")}
-            ];
-
-            router.routes = [];
-            router.map(myroutes).buildNavigationModel();
             return router.activate();
         }
     };
