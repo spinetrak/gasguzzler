686db887cf42c687edcc780c3b780790d7ef1214
spinetrak <spinetrak@spinetrak.net>
2015-03-03 20:58:38 +0100

    - enable travis ci for postgresql

--- setup_postgres.sql
+++ setup_postgres.sql
@@ -1,20 +1,21 @@
-DROP TABLE IF EXISTS "st_user" CASCADE;
-CREATE TABLE "st_user" (
-  "userid" SERIAL PRIMARY KEY,
-  "username" varchar(100) NOT NULL,
-  "password" varchar(100) NOT NULL,
-  "email" varchar(100) NOT NULL,
-  "salt" varchar(100) NOT NULL,
-  "role"    VARCHAR(100) NOT NULL,
-  "created" TIMESTAMP    NOT NULL,
-  "updated" TIMESTAMP    NOT NULL
+DROP TABLE IF EXISTS gasguzzlerdb.public.st_user CASCADE;
+CREATE TABLE gasguzzlerdb.public.st_user
+(
+  userid   SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR(100)       NOT NULL,
+  password VARCHAR(100)       NOT NULL,
+  email    VARCHAR(100)       NOT NULL,
+  salt     VARCHAR(100)       NOT NULL,
+  role     VARCHAR(100)       NOT NULL
 );
 
 
-DROP TABLE IF EXISTS "st_session" CASCADE;
-CREATE TABLE "st_session" (
-  "token" varchar(23) NOT NULL,
-  "userid" int REFERENCES "st_user" ("userid"),
-  "created" timestamp NOT NULL,
-  PRIMARY KEY ("token")
+DROP TABLE IF EXISTS gasguzzlerdb.public.st_session CASCADE;
+CREATE TABLE gasguzzlerdb.public.st_session
+(
+  token   VARCHAR(23) PRIMARY KEY NOT NULL,
+  userid  INT,
+  created TIMESTAMP               NOT NULL,
+  FOREIGN KEY (userid) REFERENCES st_user (userid)
 );
+CREATE UNIQUE INDEX st_session_userid_key ON st_session (userid);
