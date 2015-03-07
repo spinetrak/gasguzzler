d5734d696078201f76e2d6517559813b01f4567b
spinetrak <spinetrak@spinetrak.net>
2015-03-07 23:54:12 +0100

    - code cleanup  [fixes #89801370]

--- src/main/resources/assets/app/home/home.js
+++ src/main/resources/assets/app/home/home.js
@@ -23,13 +23,5 @@
  */
 
 define(function (require) {
-    var app = require('durandal/app'),
-        ko = require('knockout');
-
-    return {
-        name: ko.observable(),
-        sayHello: function () {
-            app.showMessage('Hello ' + this.name() + '! Nice to meet you.', 'Greetings', ["Close"]);
-        }
-    };
+    return {};
 });
--- src/main/resources/assets/app/site/recentstats.js
+++ src/main/resources/assets/app/site/recentstats.js
@@ -23,28 +23,5 @@
  */
 
 define(function (require) {
-    var http = require('plugins/http'),
-        ko = require('knockout');
-
-    var url = 'https://api.flickr.com/services/feeds/photos_public.gne';
-
-    var qs = {
-        tags: 'timofei',
-        tagmode: 'any',
-        format: 'json'
-    };
-
-    return {
-        images: ko.observableArray([]),
-        activate: function () {
-            var that = this;
-            if (this.images().length > 0) {
-                return;
-            }
-
-            return http.jsonp(url, qs, 'jsoncallback').then(function (response) {
-                that.images(response.items);
-            });
-        }
-    };
+    return {};
 });
