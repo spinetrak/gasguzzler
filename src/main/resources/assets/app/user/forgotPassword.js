9a698c74593c077e40c92cfef152ef7126ea01d8
spinetrak <spinetrak@spinetrak.net>
2015-03-25 22:00:09 +0100

    - password forgotten functionality [fixes #90200028]

--- src/main/resources/assets/app/user/forgotPassword.js
+++ src/main/resources/assets/app/user/forgotPassword.js
@@ -1,18 +1,18 @@
 /*
  * The MIT License (MIT)
- *
+ *  
  * Copyright (c) 2014-2015 spinetrak
- *
+ *  
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ *  
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ *  
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,29 +26,15 @@ define(function (require) {
     var http = require('plugins/http'),
         app = require('durandal/app'),
         system = require('durandal/system'),
-        shell = require('services/shell'),
         router = require('plugins/router'),
+        shell = require('services/shell'),
         ko = require('knockout');
 
-
-    var loginStatusSubscription = app.on('loggedin').then(function (loggedin, response) {
-        if (loggedin) {
-            if (response) {
-                sessionStorage.setItem("token", response.token);
-                sessionStorage.setItem("userid", response.userid);
-            }
-        }
-        else {
-            sessionStorage.removeItem("token");
-            sessionStorage.removeItem("userid");
-        }
-    });
-
     return {
-        accountScreen: ko.observable(),
-        adminScreen: ko.observable(),
-        role: ko.observable(),
+        username: ko.observable(),
+        email: ko.observable(),
         urlRoot: location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : ''),
+
 
         activate: function () {
             var userModel = {
@@ -56,35 +42,50 @@ define(function (require) {
                 "token": sessionStorage.getItem("token")
             };
 
-            if (!(userModel.userid && userModel.token)) {
+            if (userModel.userid && userModel.token) {
                 app.trigger("loggedin", false);
                 document.location.href = "/#user";
                 window.location.reload(true);
             }
+        },
 
-            var url = this.urlRoot + '/api/user/' + userModel.userid;
 
-            var that = this;
+        doRequestPasswordReset: function () {
 
-            http.get(url, '', userModel).then(function (response) {
-                    var myRole = response.role;
-                    if ("ADMIN" === myRole) {
-                        that.accountScreen('user/account');
-                        that.adminScreen('user/admin');
-                    }
-                    else {
-                        that.accountScreen('user/account');
-                        that.adminScreen('');
-                    }
-                    that.role(myRole);
+            var myemail = this.email();
+
+            var userModel = {
+                "email": myemail
+            };
+
+            if ((!myemail) || (myemail && (myemail.length < 5 || !this.doValidateEmail(myemail)))) {
+                app.showMessage("Please make sure that you have entered a valid email address", "Error!", ["Ok"], true, {"class": "notice error"});
+                return;
+            }
+
+            var url = this.urlRoot + '/api/user/pwreset';
+
+            http.post(url, userModel).then(
+                function (response) {
+                    app.showMessage("Further instructions will be sent via e-mail", "Thank you!", ["Ok"], false, {"class": "notice success"}).then(function () {
+                        app.trigger("forgotPassword", false);
+                        document.location.href = "/#user";
+                        window.location.reload(true);
+                    });
                 },
                 function (error) {
-                    app.showMessage(error.responseText, error.statusText, ["Ok"], true, {"class": "notice error"}).then(function () {
-                        app.trigger("loggedin", false);
+                    console.log(error);
+                    app.showMessage(error.statusText, "Error!", ["Ok"], false, {"class": "notice error"}).then(function () {
+                        app.trigger("forgotPassword", false);
                         document.location.href = "/#user";
                         window.location.reload(true);
                     });
                 });
+        },
+
+        doValidateEmail: function (email) {
+            var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(email);
         }
     };
 });
